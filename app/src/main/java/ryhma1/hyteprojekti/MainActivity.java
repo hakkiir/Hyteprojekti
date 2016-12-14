42728dd3d3a6fd31e53809713f18c1e962cffb8f
Petteri Sylvänne <petteri.sylvanne@metropolia.fi>
2016-12-14 12:14:27 +0200

    EditTextin syöttö Listaan

--- app/src/main/java/ryhma1/hyteprojekti/MainActivity.java
+++ app/src/main/java/ryhma1/hyteprojekti/MainActivity.java
@@ -130,17 +130,17 @@ public class MainActivity extends AppCompatActivity
         }
     */
 
-    //Navigation
-    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-    mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open_nav, R.string.close_nav);
+        //Navigation
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open_nav, R.string.close_nav);
 
-    mDrawerLayout.addDrawerListener(mToggle);
-    mToggle.syncState();
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
 
-    navigationView = (NavigationView) findViewById(R.id.nav_view);
-    navigationView.setNavigationItemSelectedListener(this);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //creating user
 
@@ -148,22 +148,20 @@ public class MainActivity extends AppCompatActivity
         List<User> users = myDb.getAllUsers();
 
         //create test user if doesn't exist
-        if(users.size() < 1){
-            myDb.addUser(new User (1, "Testi", "Henkilö", 180, 80, 0, 1, 7, 12));
+        if (users.size() < 1) {
+            myDb.addUser(new User(1, "Testi", "Henkilö", 180, 80, 0, 1, 7, 12));
         }
 
 
-
-
         //creating challenges if doesn't exist
-        List<Challenge> challenges =  myDb.getAllChallenges();
+        List<Challenge> challenges = myDb.getAllChallenges();
 
-        if (challenges.size() < 1){
-            myDb.addChallenge(new Challenge (1, "Out of bed", 1, 100, 1, 0, "9" ));
-            myDb.addChallenge(new Challenge (2, "Rocksteady", 4, 1000, 1, 0, "9" ));
-            myDb.addChallenge(new Challenge (3, "Time for bed", 1, 100, 1, 0, "9" ));
-            myDb.addChallenge(new Challenge (4, "Delicious", 1, 100, 1, 0, "9" ));
-            myDb.addChallenge(new Challenge (5, "Like a clock", 1, 100, 1, 0, "9" ));
+        if (challenges.size() < 1) {
+            myDb.addChallenge(new Challenge(1, "Out of bed", 1, 100, 1, 0, "9"));
+            myDb.addChallenge(new Challenge(2, "Rocksteady", 4, 1000, 1, 0, "9"));
+            myDb.addChallenge(new Challenge(3, "Time for bed", 1, 100, 1, 0, "9"));
+            myDb.addChallenge(new Challenge(4, "Delicious", 1, 100, 1, 0, "9"));
+            myDb.addChallenge(new Challenge(5, "Like a clock", 1, 100, 1, 0, "9"));
 
 
         }
@@ -171,8 +169,15 @@ public class MainActivity extends AppCompatActivity
         TextView haaste1 = (TextView) findViewById(R.id.haaste1);
         haaste1.setText("Tähän haasteen tiedot");*/
 
-}
 
+        Button button = (Button) findViewById(R.id.haasteButton1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startChallenge();
+            }
+        });
+    }
     public void startChallenge() {
         Challenge challenge = new Challenge();
         User user = new User();
